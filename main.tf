2273c0c63fabdebaa129400d077075a29af587fd
nadiablack <166338207+nadiablack@users.noreply.github.com>
2024-12-13 17:49:22 +0000

    Fix: Replace repository with repository_id in branch protection resources

--- main.tf
+++ main.tf
@@ -11,20 +11,18 @@ provider "github" {
   token = var.github_token
 }
 
-# Створення гілки develop
 resource "github_branch" "develop" {
   repository    = "github-terraform-task-nadiablack"
   branch        = "develop"
   source_branch = "main"
 }
 
-# Захист гілки main
 resource "github_branch_protection" "main_protection" {
-  repository                   = "github-terraform-task-nadiablack"
-  pattern                      = "main"
-  enforce_admins               = true
-  require_signed_commits       = false
-  required_linear_history      = true
+  repository_id                   = "github-terraform-task-nadiablack"
+  pattern                         = "main"
+  enforce_admins                  = true
+  require_signed_commits          = false
+  required_linear_history         = true
   require_conversation_resolution = true
 
   required_pull_request_reviews {
@@ -33,28 +31,25 @@ resource "github_branch_protection" "main_protection" {
   }
 }
 
-# Захист гілки develop
 resource "github_branch_protection" "develop_protection" {
-  repository                   = "github-terraform-task-nadiablack"
-  pattern                      = "develop"
-  enforce_admins               = false
-  require_signed_commits       = false
-  required_linear_history      = true
+  repository_id                   = "github-terraform-task-nadiablack"
+  pattern                         = "develop"
+  enforce_admins                  = false
+  require_signed_commits          = false
+  required_linear_history         = true
 
   required_pull_request_reviews {
     required_approving_review_count = 2
   }
 }
 
-# Deploy key
 resource "github_repository_deploy_key" "deploy_key" {
   repository = "github-terraform-task-nadiablack"
   title      = "DEPLOY_KEY"
-  key        = file("deploy_key.pub")
+  key        = var.deploy_key
   read_only  = false
 }
 
-# Додавання колаборатора
 resource "github_repository_collaborator" "collaborator" {
   repository = "github-terraform-task-nadiablack"
   username   = "softservedata"
