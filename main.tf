ea9e3baedd84068070d3866bd1f700000cbbd7bb
nadiablack <166338207+nadiablack@users.noreply.github.com>
2024-12-14 13:07:20 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,6 @@ terraform {
   }
 }
 
-# Оголошення змінних
 variable "github_token" {
   description = "GitHub Personal Access Token"
   type        = string
@@ -20,85 +19,41 @@ variable "deploy_key_content" {
   sensitive   = true
 }
 
-# Провайдер GitHub
+variable "repository_name" {
+  description = "Name of the existing GitHub repository"
+  type        = string
+}
+
 provider "github" {
   token = var.github_token
 }
 
-# Створення репозиторію
-resource "github_repository" "my_repo" {
-  name        = "github-terraform-task-nadiablack"
-  description = "Repository managed by Terraform"
-  visibility  = "public"
-  auto_init   = true
+data "github_repository" "existing_repo" {
+  name = var.repository_name
 }
 
-# Додавання співпрацівника
+resource "github_branch" "main" {
+  repository = data.github_repository.existing_repo.name
+  branch     = "main"
+}
+
 resource "github_repository_collaborator" "collaborator" {
-  repository = github_repository.my_repo.name
+  repository = data.github_repository.existing_repo.name
   username   = "softservedata"
   permission = "push"
 }
 
-# Створення гілки develop
-resource "github_branch" "develop" {
-  repository = github_repository.my_repo.name
-  branch     = "develop"
-}
-
-# Встановлення develop як гілки за замовчуванням
-resource "github_branch_default" "default" {
-  repository = github_repository.my_repo.name
-  branch     = github_branch.develop.branch
-}
-
-# Захист гілки main
-resource "github_branch_protection" "main" {
-  repository_id = github_repository.my_repo.node_id
-  pattern       = "main"
-  enforce_admins = true
-
-  required_pull_request_reviews {
-    dismiss_stale_reviews           = true
-    require_code_owner_reviews      = true
-    required_approving_review_count = 1
-  }
-
-  required_status_checks {
-    strict   = true
-    contexts = []
-  }
-}
-
-# Захист гілки develop
-resource "github_branch_protection" "develop" {
-  repository_id = github_repository.my_repo.node_id
-  pattern       = "develop"
-  enforce_admins = true
-
-  required_pull_request_reviews {
-    dismiss_stale_reviews           = true
-    require_code_owner_reviews      = false
-    required_approving_review_count = 2
-  }
-
-  required_status_checks {
-    strict   = true
-    contexts = []
-  }
-}
-
-# Додавання файлу CODEOWNERS
 resource "github_repository_file" "codeowners" {
-  repository = github_repository.my_repo.name
+  depends_on = [github_branch.main]
+  repository = data.github_repository.existing_repo.name
   file       = ".github/CODEOWNERS"
   content    = "* @softservedata"
   branch     = "main"
 }
 
-# Шаблон для Pull Request
 resource "github_repository_file" "pull_request_template" {
-  repository = github_repository.my_repo.name
+  depends_on = [github_branch.main]
+  repository = data.github_repository.existing_repo.name
   file       = ".github/pull_request_template.md"
   content = <<-EOT
     ## Describe your changes
@@ -115,9 +70,8 @@ resource "github_repository_file" "pull_request_template" {
   branch = "main"
 }
 
-# Додавання deploy key
 resource "github_repository_deploy_key" "deploy_key" {
-  repository = github_repository.my_repo.name
+  repository = data.github_repository.existing_repo.name
   title      = "DEPLOY_KEY"
   key        = var.deploy_key_content
   read_only  = false
